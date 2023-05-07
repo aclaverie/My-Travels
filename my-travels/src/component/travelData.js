d87ffce860082b7123b7a315ec15d46f61f59723
Anthony Claverie <44935840+aclaverie@users.noreply.github.com>
2023-05-07 17:59:20 -0400

    Travel Data File created

---
+++ my-travels/src/component/travelData.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        title: "San Fernando Hill",
+        location: "San Fernando",
+        googleMapsURL: "https://goo.gl/maps/vMu2nJtJ9LaeWfyH8?coh=178573&entry=tt",
+        startDate: "13th August 2019",
+        endDate: "17th August 2019",
+        description: "This hill is in my home town. My family frequently picnic here. It is very scenic and peaceful. The venue can host weddings and concerts. It is a must see if visiting South Trinidad.",
+        imageURL: "san-fernando-hill.jpg"        
+    },
+    {
+        title: "Queen's Hall",
+        location: "Saint Ann's Road, Port of Spain",
+        googleMapsURL: "https://goo.gl/maps/P5FvuaBUYEVJCRGs6?coh=178573&entry=tt",
+        startDate: "October 6, 2019",
+        endDate: "October 10, 2019",
+        description: "I  attended a dance concert here and loved it. It was so well organised and clearly the technical support staff rehearse properly so there are no hiccups on the day! They start on-time here; not Trini-time so this is a great example to set to other performing arts venues.",
+        imageURL: "queen-s-hall.jpg"        
+    },
+    {
+        title: "Ortinola Estates",
+        location: "Maracas Valley, St Joseph, Trinidad",
+        googleMapsURL: "https://goo.gl/maps/gDJS5qadvr5DRjHX7?coh=178573&entry=tt",
+        startDate: "October 4th, 2020",
+        endDate: "October 8th, 2020",
+        description: "We had an absolutely amazing experience at this beautiful place. People running the estate are so welcoming, friendly and helpful and we have enjoyed every minute of our chocolate making tour.",
+        imageURL: "ortinola-great-house.jpg"        
+    },
+    {
+        title: "Icacos Point",
+        location: "Cedros, Trinidad",
+        googleMapsURL: "https://goo.gl/maps/gpxiDTVYRAUZvAZc8?coh=178573&entry=tt",
+        startDate: "November 5th, 2019",
+        endDate: "November 9th, 2019",
+        description: "This remains one of the villages of Trinidad. Close to venezuela, there is much to experience. Long drive on narrow roads",
+        imageURL: "icacos-point.jpg"        
+    },
+    {
+        title: "Independence Square",
+        location: "Independence Square, Port Of Spain, Trinidad",
+        googleMapsURL: "https://goo.gl/maps/Ugk3WVnvTaBqudtp6?coh=178573&entry=tt",
+        startDate: "February 26, 2019",
+        endDate: "March 3rd, 2019",
+        description: "Independence Square also called Brian Lara Promenade learning the history of Trinidad's independence. The square is surrounded by government buildings including finance, courts, treasury and banks. The food market is up the street where you can buy all your fresh vegetables and fruit.",
+        imageURL: "the-square.jpg"        
+    }
+]
